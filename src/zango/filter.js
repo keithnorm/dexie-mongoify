e2111afd193db6d785cf806f878bd753b6e7e52c
Keith Norman <keithnorm@gmail.com>
2017-07-18 09:26:45 -0700

    fix filter when used with $lookup in aggregate

--- src/zango/filter.js
+++ src/zango/filter.js
@@ -2,10 +2,10 @@ const Fields = require('./lang/fields.js');
 
 module.exports = (next, pred) => (cb) => {
     (function iterate() {
-        next((error, doc, idb_cur) => {
+        next((error, doc, idb_cur, idb_transaction) => {
             if (!doc) { cb(error); }
             else if (pred.run(new Fields(doc))) {
-                cb(null, doc, idb_cur);
+                cb(null, doc, idb_cur, idb_transaction);
             } else { iterate(); }
         });
     })();
